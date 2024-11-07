e19627c42e60ec8a21364a9f2dfe2a66c3169d64
Paweł <114557817+Pawi1@users.noreply.github.com>
2024-11-07 18:08:58 +0100

    Update Program.cs

--- Testowanie/Program.cs
+++ Testowanie/Program.cs
@@ -4,9 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("ZADANIE HUFFMAN");
-        Console.Write("Podaj ciąg do zakodowania Huffmanem: ");
-        string input = Console.ReadLine()!;
-        Console.WriteLine($"Twój Huffman: {Huffman.Encode(input)}");;
+        Huffman.Exercise2()
     }
 }
