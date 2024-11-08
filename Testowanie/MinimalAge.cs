473c4ac00a0c074307b08b414ee6733c6400ae51
pawi1 <p.rutkow@o2.pl>
2024-11-08 14:36:32 +0100

    dodanie zadanie 5

---
+++ Testowanie/MinimalAge.cs
@@ -0,0 +1,28 @@
+namespace Testowanie;
+public class MinimalAge
+{
+    public static byte CalculateMinimalAge(byte age)
+    {
+        return (byte)(age / 2 + 10);
+    }
+    public static int Exercise3()
+    {
+        try
+        {
+            Console.Write("Podaj swój wiek: ");
+            byte age = Convert.ToByte(Console.ReadLine()??"");
+            Console.WriteLine($"Minimalny wiek osoby z którą możesz się umówić wynosi: {CalculateMinimalAge(age)}");
+            return 0;
+        }
+        catch(OverflowException)
+        {
+            Console.WriteLine("Musisz podałeś swój wiek w zakresie: (0-255).");
+            return 1;
+        }
+        catch(FormatException)
+        {
+            Console.WriteLine("Twój wiek musi być liczbą.");
+            return 1;
+        }
+    }
+}
