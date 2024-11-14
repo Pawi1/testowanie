4416ce0b8c2766842f68ccc63360576f0e9d2072
Paweł <114557817+Pawi1@users.noreply.github.com>
2024-11-14 10:37:11 +0100

    Update run_test.py

--- run_test.py
+++ run_test.py
@@ -1,6 +1,9 @@
 from subprocess import run
+from os import path
 
+if not paht.isdir("./Testowanie.Tests/bin"):
+    run(["python", "build.py"])
 run(
-    ["dotnet", "test", " --logger", "console;verbosity=detailed"],
-    cwd="Testowanie.Tests",
+    ["dotnet", "test", "--logger", "console;verbosity=detailed"],
+    cwd="Testowanie.Tests"
 )
