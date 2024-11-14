bdb8e2956ac2fcada633926f881ea34ece9d29c9
Paweł <114557817+Pawi1@users.noreply.github.com>
2024-11-14 11:28:11 +0100

    Update run_test.py

--- run_test.py
+++ run_test.py
@@ -2,7 +2,8 @@ from subprocess import run
 from os import path
 
 if not path.isdir("./Testowanie.Tests/bin"):
-    run(["python", "build.py"])
+   if path.exist("build.py") 
+       run(["python", "build.py"])
 run(
     ["dotnet", "test", "--logger", "console;verbosity=detailed"],
     cwd="Testowanie.Tests"
